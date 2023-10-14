43ac65b5590226ebc7dd0650896de452791bbcfc
BigManRoss <bigmanrosssteam@gmail.com>
2023-10-14 16:34:44 +0200

    Changed port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
-    await app.listen(3000);
+    await app.listen(process.env.PORT, "0.0.0.0");
     console.log("Server started at http://localhost:3000");
 }
 bootstrap();
