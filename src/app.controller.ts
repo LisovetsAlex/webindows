c1cb032ee951a109cfb1a575063779b8ad622b57
BigManRoss <bigmanrosssteam@gmail.com>
2023-10-15 12:52:36 +0200

    fixed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,7 +9,7 @@ export class AppController {
 
     @Get("/:filepath(*)")
     getFile(@Param() params: any, @Req() request: Request, @Res() res: Response) {
-        const fileName = params.filepath === "" ? "frontend/index.html" : "frontend/" + params.filepath;
+        const fileName = params.filepath === "" ? "webindows/frontend/index.html" : "webindows/frontend/" + params.filepath;
         const fullUrl = this.appService.transformFilePath(join(process.cwd(), fileName));
         console.log("LOG: fullUrl: " + fullUrl);
         console.log("LOG: req: " + request.originalUrl);
