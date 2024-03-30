2f8ec44b67d09aa75e2f6aa3688d9267b5300495
BigManRoss <bigmanrosssteam@gmail.com>
2024-03-30 16:24:06 +0100

    feat: created dockerfile for production build in docker

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,7 +4,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
     entry: "./index.js",
     output: {
-        path: path.resolve(__dirname, "../fe-build"),
+        path: path.resolve(__dirname, "./build"),
         filename: "bundle.js",
     },
     mode: "production",
